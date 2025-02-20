d82ac5575b5a0c858df649684a9d47f5ae85ea66
Dejan Jovic <dejanjovicjovic5@gmail.com>
2025-02-20 15:18:09 +0100

    Added function for removing a product

--- product_manager.py
+++ product_manager.py
@@ -12,3 +12,5 @@ class ProductManager:
     def total_inventory_value(self):
         return sum(product.price * product.quantity for product in self.products)
     
+    def remove_product(self, name):
+        self.products = [product for product in self.products if product.name != name]
